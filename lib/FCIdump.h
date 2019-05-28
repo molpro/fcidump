05a94283c381aa1e4026dd22e55cdb96fcd4c67e
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2019-05-28 16:40:41 +0100

    ascii

--- lib/FCIdump.h
+++ lib/FCIdump.h
@@ -15,7 +15,7 @@
  @section Introduction
  This is a C++ class, together with non-object-oriented C and Fortran 90 bindings, that implement access
  to an FCIDUMP file as produced by the Full CI code (Computer Physics Communications
-Volume 54, Issue 1, April 1989, Pages 75–83,
+Volume 54, Issue 1, April 1989, Pages 75-83,
 <a href="http://dx.doi.org/10.1016/0010-4655(89)90033-7">
 doi:10.1016/0010-4655(89)90033-7</a>;
 <a href="http://bitbucket.org:pjknowles/fci">
